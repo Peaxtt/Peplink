29e3671e46e752f41af25293d6b6ce6c3f82e5d9
Peaxtt <155731255+Peaxtt@users.noreply.github.com>
2026-03-07 11:28:34 +0700

    fix: explicit tmux pane targeting

--- start_peplink.sh
+++ start_peplink.sh
@@ -64,36 +64,36 @@ echo "Starting Peplink GPS and Alignment nodes in a new tmux session..."
 tmux new-session -d -s $SESSION_NAME -n "system"
 
 # Split horizontally (creates Pane 1 at bottom)
-tmux split-window -v -p 40
+tmux split-window -t $SESSION_NAME:0.0 -v -p 40
 
 # Split the bottom pane vertically (creates Pane 2 at bottom right)
-tmux split-window -h
+tmux split-window -t $SESSION_NAME:0.1 -h
 
-# --- Pane 0: Peplink GPS Node ---
-tmux send-keys -t $SESSION_NAME:0 "cd \"$DIR\"" C-m
-tmux send-keys -t $SESSION_NAME:0 "source /opt/ros/humble/setup.bash" C-m
-tmux send-keys -t $SESSION_NAME:0 "source install/setup.bash" C-m
-tmux send-keys -t $SESSION_NAME:0 "ros2 run peplink_gps_driver peplink_gps_node" C-m
+# --- Pane 0 (Top): Peplink GPS Node ---
+tmux send-keys -t $SESSION_NAME:0.0 "cd \"$DIR\"" C-m
+tmux send-keys -t $SESSION_NAME:0.0 "source /opt/ros/humble/setup.bash" C-m
+tmux send-keys -t $SESSION_NAME:0.0 "source install/setup.bash" C-m
+tmux send-keys -t $SESSION_NAME:0.0 "ros2 run peplink_gps_driver peplink_gps_node" C-m
 
-# --- Pane 1: PTTEP Alignment Node ---
-tmux send-keys -t $SESSION_NAME:1 "cd \"$DIR\"" C-m
-tmux send-keys -t $SESSION_NAME:1 "source /opt/ros/humble/setup.bash" C-m
-tmux send-keys -t $SESSION_NAME:1 "source install/setup.bash" C-m
-tmux send-keys -t $SESSION_NAME:1 "ros2 run pttep_alignment alignment_node" C-m
+# --- Pane 1 (Bottom Left): PTTEP Alignment Node ---
+tmux send-keys -t $SESSION_NAME:0.1 "cd \"$DIR\"" C-m
+tmux send-keys -t $SESSION_NAME:0.1 "source /opt/ros/humble/setup.bash" C-m
+tmux send-keys -t $SESSION_NAME:0.1 "source install/setup.bash" C-m
+tmux send-keys -t $SESSION_NAME:0.1 "ros2 run pttep_alignment alignment_node" C-m
 
-# --- Pane 2: User Commands (Services) ---
-tmux send-keys -t $SESSION_NAME:2 "cd \"$DIR\"" C-m
-tmux send-keys -t $SESSION_NAME:2 "source /opt/ros/humble/setup.bash" C-m
-tmux send-keys -t $SESSION_NAME:2 "source install/setup.bash" C-m
-tmux send-keys -t $SESSION_NAME:2 "clear" C-m
-tmux send-keys -t $SESSION_NAME:2 "echo '=== ALIGNMENT COMMANDS ==='" C-m
-tmux send-keys -t $SESSION_NAME:2 "echo '1. To save a point (do this 3+ times at diff locations):'" C-m
-tmux send-keys -t $SESSION_NAME:2 "echo '   ros2 service call /save_location std_srvs/srv/Trigger'" C-m
-tmux send-keys -t $SESSION_NAME:2 "echo '2. To calculate and start real-time aligned odometry:'" C-m
-tmux send-keys -t $SESSION_NAME:2 "echo '   ros2 service call /calculate_transformation pttep_alignment/srv/CalculateTransformation \"{reset: false}\"'" C-m
-tmux send-keys -t $SESSION_NAME:2 "echo '3. To view the aligned output:'" C-m
-tmux send-keys -t $SESSION_NAME:2 "echo '   ros2 topic echo /aligned_odom'" C-m
-tmux send-keys -t $SESSION_NAME:2 "" C-m
+# --- Pane 2 (Bottom Right): User Commands (Services) ---
+tmux send-keys -t $SESSION_NAME:0.2 "cd \"$DIR\"" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "source /opt/ros/humble/setup.bash" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "source install/setup.bash" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "clear" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "echo '=== ALIGNMENT COMMANDS ==='" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "echo '1. To save a point (do this 3+ times at diff locations):'" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "echo '   ros2 service call /save_location std_srvs/srv/Trigger'" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "echo '2. To calculate and start real-time aligned odometry:'" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "echo '   ros2 service call /calculate_transformation pttep_alignment/srv/CalculateTransformation \"{reset: false}\"'" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "echo '3. To view the aligned output:'" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "echo '   ros2 topic echo /aligned_odom'" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "" C-m
 
 # We will check if we are in a headless environment. If we are, we don't attach.
 if [ -t 0 ]; then
